12713d2a44862a3e3f364c85c374b8244973893b
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-10-07 21:12:57 +0000

    Add files via upload

--- React-Router/pages/About.jsx
+++ React-Router/pages/About.jsx
@@ -1,2 +1,29 @@
-1
+import { HashLink } from 'react-router-hash-link';
 
+export default function About({ isHome }) {
+  if (isHome) {
+    return (
+      <div id="about">
+        <h1>About Inside Home</h1>
+        <section style={{ marginBottom: '50rem' }}>
+          <HashLink
+            to="/#home"
+            smooth
+            scroll={(el) => {
+              console.log(el);
+              el.scrollIntoView({ behavior: 'auto', block: 'end' });
+            }}
+          >
+            {' '}
+            Back To The Top
+          </HashLink>
+        </section>
+      </div>
+    );
+  }
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
